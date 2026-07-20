ec12424516ae0304228dd2321d4e604fe225e04e
Jie Lu <wx_lujie@163.com>
2026-07-20 21:59:05 +0800

    update decode jsons and writereg

--- model_execplan/src/execution_plan_generator/control_registers.py
+++ model_execplan/src/execution_plan_generator/control_registers.py
@@ -1739,7 +1739,7 @@ def _compute_decode_add_fp16N_fp32N_fp16N_control_register_updates(
     return {
 
         "iga_lc0.dram_loop_configs.end": d_n // 16 if a_n is not None else 0,
-        "iga_lc4.dram_loop_configs.end": d_n // 16 if d_n is not None else 0,
+        "iga_lc4.dram_loop_configs.end": d_n // 8 if d_n is not None else 0,
     }
 
 def _compute_decode_mul_fp32N_fp32N_fp16N_control_register_updates(
